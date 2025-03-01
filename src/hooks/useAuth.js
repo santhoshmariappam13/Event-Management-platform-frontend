1fd04c8fb0cd51dfef01dc52a42b23f0b0aa7006
santhosh <santhoshmariappan13@gmail.com>
2025-03-01 22:09:23 +0530

    Update useAuth.js

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,10 +1,10 @@
 import { useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
 
-// Define the useAuth hook
+
 const useAuth = () => {
   return useContext(AuthContext);
 };
 
-// Default export of the hook
+
 export default useAuth;
